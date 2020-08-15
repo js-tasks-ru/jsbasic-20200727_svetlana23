69f2a6a8a53718328851e60cbfdb04c37766ac48
Svetlana <svetlanakrbva@rambler.ru>
2020-08-15 23:44:09 +0300

    Решена 2-task 4-module

---
+++ 4-module/2-task/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {HTMLTableElement} table
+ * @return {void}
+ */
+function makeDiagonalRed(table) {
+    let row = table.rows;
+    return Array.from(row).map (function (item, ind) {
+      let cell = item.cells;
+      cell[ind].style.backgroundColor = "red";
+    });
+  }
